9475288ce0320766216972adfad0a98e43697355
Jurso28 <josephurso28@gmail.com>
2022-02-18 16:26:38 -0600

    Update

---
+++ java.js
@@ -0,0 +1,216 @@
+function myFunction1() {
+    var x;
+        let firstName = prompt("First Name: ")
+        let lastName = prompt("Last Name: ")
+        console.log(firstName);
+        console.log(lastName);
+        document.getElementById("Box1").innerHTML = firstName + " " + lastName;
+
+    }
+function myFunction2() {
+    var x;
+        let firstName = prompt("First Name: ")
+        let lastName = prompt("Last Name: ")
+        console.log(firstName);
+        console.log(lastName);
+        document.getElementById("Box2").innerHTML = firstName + " " + lastName;
+
+        }
+function myFunction3() {
+    var x;
+        let firstName = prompt("First Name: ")
+        let lastName = prompt("Last Name: ")
+        console.log(firstName);
+        console.log(lastName);
+        document.getElementById("Box3").innerHTML = firstName + " " + lastName;
+
+        }
+function myFunction4() {
+    var x;
+        let firstName = prompt("First Name: ")
+        let lastName = prompt("Last Name: ")
+        console.log(firstName);
+        console.log(lastName);
+        document.getElementById("Box4").innerHTML = firstName + " " + lastName;
+
+        }
+function myFunction5() {
+    var x;
+        let firstName = prompt("First Name: ")
+        let lastName = prompt("Last Name: ")
+        console.log(firstName);
+        console.log(lastName);
+        document.getElementById("Box5").innerHTML = firstName + " " + lastName;
+
+        }
+function myFunction6() {
+    var x;
+        let firstName = prompt("First Name: ")
+        let lastName = prompt("Last Name: ")
+        console.log(firstName);
+        console.log(lastName);
+        document.getElementById("Box6").innerHTML = firstName + " " + lastName;
+
+        }
+function myFunction7() {
+    var x;
+        let firstName = prompt("First Name: ")
+        let lastName = prompt("Last Name: ")
+        console.log(firstName);
+        console.log(lastName);
+        document.getElementById("Box7").innerHTML = firstName + " " + lastName;
+
+        }
+function myFunction8() {
+    var x;
+        let firstName = prompt("First Name: ")
+        let lastName = prompt("Last Name: ")
+        console.log(firstName);
+        console.log(lastName);
+        document.getElementById("Box8").innerHTML = firstName + " " + lastName;
+
+        }
+function myFunction9() {
+    var x;
+        let firstName = prompt("First Name: ")
+        let lastName = prompt("Last Name: ")
+        console.log(firstName);
+        console.log(lastName);
+        document.getElementById("Box9").innerHTML = firstName + " " + lastName;
+
+        }
+function myFunction10() {
+    var x;
+        let firstName = prompt("First Name: ")
+        let lastName = prompt("Last Name: ")
+        console.log(firstName);
+        console.log(lastName);
+        document.getElementById("Box10").innerHTML = firstName + " " + lastName;
+
+        }
+function myFunction11() {
+    var x;
+        let firstName = prompt("First Name: ")
+        let lastName = prompt("Last Name: ")
+        console.log(firstName);
+        console.log(lastName);
+        document.getElementById("Box11").innerHTML = firstName + " " + lastName;
+
+        }
+function myFunction12() {
+    var x;
+        let firstName = prompt("First Name: ")
+        let lastName = prompt("Last Name: ")
+        console.log(firstName);
+        console.log(lastName);
+        document.getElementById("Box12").innerHTML = firstName + " " + lastName;
+
+        }
+function myFunction13(){
+    var x;
+        let firstName = prompt("First Name: ")
+        let lastName = prompt("Last Name: ")
+        console.log(firstName);
+        console.log(lastName);
+        document.getElementById("Box13").innerHTML = firstName + " " + lastName;
+
+        }
+function myFunction14() {
+    var x;
+        let firstName = prompt("First Name: ")
+        let lastName = prompt("Last Name: ")
+        console.log(firstName);
+        console.log(lastName);
+        document.getElementById("Box14").innerHTML = firstName + " " + lastName;
+
+        }
+function myFunction15() {
+    var x;
+        let firstName = prompt("First Name: ")
+        let lastName = prompt("Last Name: ")
+        console.log(firstName);
+        console.log(lastName);
+        document.getElementById("Box15").innerHTML = firstName + " " + lastName;
+
+        }
+function myFunction16() {
+    var x;
+        let firstName = prompt("First Name: ")
+        let lastName = prompt("Last Name: ")
+        console.log(firstName);
+        console.log(lastName);
+        document.getElementById("Box16").innerHTML = firstName + " " + lastName;
+
+        }
+function myFunction17() {
+    var x;
+        let firstName = prompt("First Name: ")
+        let lastName = prompt("Last Name: ")
+        console.log(firstName);
+        console.log(lastName);
+        document.getElementById("Box17").innerHTML = firstName + " " + lastName;
+
+        }
+function myFunction18() {
+    var x;
+        let firstName = prompt("First Name: ")
+        let lastName = prompt("Last Name: ")
+        console.log(firstName);
+        console.log(lastName);
+        document.getElementById("Box18").innerHTML = firstName + " " + lastName;
+
+        }
+function myFunction19() {
+    var x;
+        let firstName = prompt("First Name: ")
+        let lastName = prompt("Last Name: ")
+        console.log(firstName);
+        console.log(lastName);
+        document.getElementById("Box19").innerHTML = firstName + " " + lastName;
+
+        }
+function myFunction20() {
+    var x;
+        let firstName = prompt("First Name: ")
+        let lastName = prompt("Last Name: ")
+        console.log(firstName);
+        console.log(lastName);
+        document.getElementById("Box20").innerHTML = firstName + " " + lastName;
+
+        }
+function myFunction21() {
+    var x;
+        let firstName = prompt("First Name: ")
+        let lastName = prompt("Last Name: ")
+        console.log(firstName);
+        console.log(lastName);
+        document.getElementById("Box21").innerHTML = firstName + " " + lastName;
+
+        }
+function myFunction22() {
+    var x;
+        let firstName = prompt("First Name: ")
+        let lastName = prompt("Last Name: ")
+        console.log(firstName);
+        console.log(lastName);
+        document.getElementById("Box22").innerHTML = firstName + " " + lastName;
+
+        }
+function myFunction23() {
+    var x;
+        let firstName = prompt("First Name: ")
+        let lastName = prompt("Last Name: ")
+        console.log(firstName);
+        console.log(lastName);
+        document.getElementById("Box23").innerHTML = firstName + " " + lastName;
+
+        }
+function myFunction24() {
+    var x;
+        let firstName = prompt("First Name: ")
+        let lastName = prompt("Last Name: ")
+        console.log(firstName);
+        console.log(lastName);
+        document.getElementById("Box24").innerHTML = firstName + " " + lastName;
+
+        }
