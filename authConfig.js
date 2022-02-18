c41f8d5f62cfe9ee4627a0f571dd11d8a9f0d9c9
Jurso28 <josephurso28@gmail.com>
2022-02-18 00:38:53 -0600

    uploading all the files

---
+++ authConfig.js
@@ -0,0 +1,21 @@
+const msalConfig = {
+    auth: {
+      clientId: "44dff28f-da6f-4754-bdd2-1453b36da44f",
+      authority: "https://login.microsoftonline.com/common",
+      redirectUri: "https://capstone-is-test-site.azurewebsites.net/",
+    },
+    cache: {
+      cacheLocation: "sessionStorage", // This configures where your cache will be stored
+      storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+    }
+  };
+
+  // Add here scopes for id token to be used at MS Identity Platform endpoints.
+  const loginRequest = {
+    scopes: ["openid", "profile", "User.Read"]
+  };
+
+  // Add here scopes for access token to be used at MS Graph API endpoints.
+  const tokenRequest = {
+    scopes: ["Mail.Read"]
+  };
